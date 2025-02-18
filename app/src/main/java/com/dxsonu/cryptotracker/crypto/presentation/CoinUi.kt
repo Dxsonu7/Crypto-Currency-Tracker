49b4ecc7331e44cb2af764c7257f7538179fdf01
Sonu Gupta <sonu72752@gmail.com>
2025-02-18 01:14:05 -0600

    Coin UI and Number Formatter

--- app/src/main/java/com/dxsonu/cryptotracker/crypto/presentation/CoinUi.kt
+++ app/src/main/java/com/dxsonu/cryptotracker/crypto/presentation/CoinUi.kt
@@ -1,6 +1,11 @@
 package com.dxsonu.cryptotracker.crypto.presentation
 
+import android.icu.number.NumberFormatter
 import androidx.annotation.DrawableRes
+import com.dxsonu.cryptotracker.crypto.domain.Coin
+import com.dxsonu.cryptotracker.util.getDrawableIdForCoin
+import java.text.NumberFormat
+import java.util.Locale
 
 data class CoinUi(
     val id: String,
@@ -17,3 +22,27 @@ data class DisplayableNumber(
     val value: Double,
     val formatted: String
 )
+
+fun Coin.toCoinUi(): CoinUi {
+    return CoinUi(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hr = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
