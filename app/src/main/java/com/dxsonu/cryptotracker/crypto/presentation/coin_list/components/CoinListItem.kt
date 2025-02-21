8c49471c81a10602c69668a7b7552917b6e0f65a
Sonu Gupta <sonu72752@gmail.com>
2025-02-20 22:39:08 -0600

    mobile theme specific text font impl

--- app/src/main/java/com/dxsonu/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/dxsonu/cryptotracker/crypto/presentation/coin_list/components/CoinListItem.kt
@@ -2,21 +2,27 @@ package com.dxsonu.cryptotracker.crypto.presentation.coin_list.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewDynamicColors
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dxsonu.cryptotracker.crypto.domain.Coin
 import com.dxsonu.cryptotracker.crypto.presentation.CoinUi
 import com.dxsonu.cryptotracker.crypto.presentation.toCoinUi
@@ -28,6 +34,11 @@ fun CoinListItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val contentColor = if(isSystemInDarkTheme()) {
+        Color.White
+    } else {
+        Color.Black
+    }
     Row(
         modifier = modifier
             .clickable(onClick = onClick)
@@ -41,6 +52,16 @@ fun CoinListItem(
             tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier.size(85.dp)
         )
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = coinUi.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = contentColor
+            )
+        }
     }
 }
 
